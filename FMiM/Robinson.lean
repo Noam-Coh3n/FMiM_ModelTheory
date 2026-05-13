796773c11994abc62457610aa00c1adaa7724c24
Noam Cohen <noam@cohen.nl>
2026-05-14 00:38:34 +0200

    Stated Robinson's Theorem

--- FMiM/Robinson.lean
+++ FMiM/Robinson.lean
@@ -1,10 +1,13 @@
 import FMiM.ElementarySystems
+import Mathlib.ModelTheory.Satisfiability
 
-open FirstOrder
+open FirstOrder Language
 
 variable {L E₁ E₂ : Language}
 
-abbrev L₁ := L.sum E₁
-abbrev L₂ := L.sum E₂
 
-variable {T : L.Theory} {T₁ : L₁.Theory} {T₂ : L₂.Theory}
+variable {T : L.Theory} {T₁ : (L.sum E₁).Theory} {T₂ : (L.sum E₂).Theory}
+variable {T_sub₁ : LHom.sumInl.onTheory T ⊆ T₁} {T_sub₂ : LHom.sumInl.onTheory T ⊆ T₂}
+variable {T_complete : T.IsComplete}
+
+theorem Robinson (sat₁ : T₁.IsSatisfiable) (sat₂ : T₂.IsSatisfiable) : T.IsSatisfiable := sorry
